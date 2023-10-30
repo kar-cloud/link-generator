82a1f633b22e4719cccc930230890bef3467f8bf
karan_mann <karan.mann@travclan.com>
2023-10-30 20:17:34 +0530

    changes

--- link_generator/settings.py
+++ link_generator/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = '{}/media/'.format(BASE_DIR)
 MEDIA_URL = '/media/'
